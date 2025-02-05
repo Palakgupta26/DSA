e2b9e127219d804c384be41aa486367cd1d490d3
Palakgupta26 <Palakg431@gmail.com>
2025-02-05 10:57:53 +0530

    sub sequence

---
+++ day29/SubSequence.java
@@ -0,0 +1,18 @@
+package day29;
+
+public class SubSequence {
+    public static void main(String[] args) {
+        subSequence("","abc");
+    }
+    static void subSequence(String p, String up){
+        if(up.isEmpty()){
+            System.out.print(p+" ");
+            return;
+        }
+        char ch = up.charAt(0);
+
+        subSequence(p+ch,up.substring(1));
+        subSequence(p,up.substring(1));
+    }
+
+}
