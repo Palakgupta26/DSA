d9d4159924db7a62e051a96ad8778ed58a08e117
Palakgupta26 <Palakg431@gmail.com>
2025-06-30 02:29:24 +0530

    tree

---
+++ pq.java
@@ -0,0 +1,15 @@
+public class pq {
+    public static void main(String[] args) {
+
+    }
+    int maxCost(int N, int[] arr){
+        int max=1;
+        int j=1;
+        int cost=N-1;
+        for(int i=1; i<=N;i++){
+            if(arr[i]%arr[j]==0){
+
+            }
+        }
+    }
+}
